e2eef7226b5b3b0d1125b9a1c933b96df05584cb
RIZQI RAMA HAYUSTYA <73214485+kikikhayst@users.noreply.github.com>
2021-10-20 08:05:05 +0700

    Pertemuan 3 dan 4 (Sesi 1 & Sesi 2) (Tugas 1)
    Memfungsikan tiap tombol dibawah dan menampilkan Tab Data Siswa, Data Kelas, dan Data Guru.

--- java/com/sinus/projectaplikasi/MainActivity.kt
+++ java/com/sinus/projectaplikasi/MainActivity.kt
@@ -2,8 +2,12 @@ package com.sinus.projectaplikasi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.sinus.projectaplikasi.fragment.AkunFragment
 import com.sinus.projectaplikasi.fragment.BarangFragment
 import com.sinus.projectaplikasi.fragment.HomeFragment
@@ -13,7 +17,11 @@ class MainActivity : AppCompatActivity() {
     val fragmentbarang : Fragment = BarangFragment()
     val fragmentakun : Fragment = AkunFragment()
     val fm : FragmentManager = supportFragmentManager
-    val active : Fragment = HomeFragment()
+    var active : Fragment = HomeFragment()
+
+    private lateinit var menu : Menu
+    private lateinit var menuItem : MenuItem
+    private lateinit var bottomNavigationView: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,5 +30,36 @@ class MainActivity : AppCompatActivity() {
         fm.beginTransaction().add(R.id.nav_host_fragment, fragmenthome).show(fragmenthome).commit()
         fm.beginTransaction().add(R.id.nav_host_fragment, fragmentbarang).hide(fragmentbarang).commit()
         fm.beginTransaction().add(R.id.nav_host_fragment, fragmentakun).hide(fragmentakun).commit()
+
+        bottomNavigationView = findViewById(R.id.nav_view)
+        menu = bottomNavigationView.menu
+        menuItem = menu.getItem(0)
+        menuItem.isChecked = true
+
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.navigation_home-> {
+                    Log.d("Respon", "Home")
+                    menuItem = menu.getItem(0)
+                    menuItem.isChecked = true
+                    fm.beginTransaction().hide(active).show(fragmenthome).commit()
+                    active = fragmenthome
+                }
+                R.id.navigation_barang-> {
+                    Log.d("Respon", "Barang")
+                    menuItem = menu.getItem(1)
+                    menuItem.isChecked = true
+                    fm.beginTransaction().hide(active).show(fragmentbarang).commit()
+                    active = fragmentbarang
+                }
+                R.id.navigation_akun-> {
+                    Log.d("Respon", "Akun")
+                    menuItem = menu.getItem(2)
+                    menuItem.isChecked = true
+                    fm.beginTransaction().hide(active).show(fragmentakun).commit()
+                    active = fragmentakun
+                }
+            }
+        false }
     }
 }
